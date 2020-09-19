70a61c02d6328ce0ed3fb6ee9caf2c312819feb4
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2020-09-18 19:04:29 -0500

    Add files via upload

--- showwcase.R
+++ showwcase.R
@@ -9,15 +9,26 @@ sd(data$session_actions)
 
 #line graph for total session actions per day
 sessionActionss = aggregate(data$customer_id, by=list(data$session_actions), FUN=sum)
-OctoberActions = aggregate(data$session_actions, by=list(data$login_date), FUN=sum)
+OctoberActions = aggregate(data$login_date, by=list(data$session_action), FUN=sum)
 plot(OctoberActions$Group.1, OctoberActions$x,type = "o",main="Session Actions per Day in October",xlab="Date", ylab="Total Session Actions")
 
 #stacked bar graph for active duration
 data$activeDuration <- data$session_duration - data$inactive_duration
 data$activeDuration <- ifelse(data$activeDuration < 0, 0, data$activeDuration)
-userActiveDuration = aggregate(data$customer_id, by=list(data$activeDuration), FUN=sum)
-
+userActiveDuration = aggregate(cbind(active= data$activeDuration,inactive = data$inactive_duration), by = list(Customer = data$customer_id), sum)
+userActiveDuration$Customer <- 1:nrow(userActiveDuration) 
 install.packages("ggplot2")
 library("ggplot2")
-ggplot(data, aes(fill=session_duration, y=userActiveDuration$x, x=userActiveDuration$Group.1)) + geom_bar(position="stack", stat="identity")
+activeChart = barplot(userActiveDuration$active,name = userActiveDuration$Customer,main="Active Duration per User",xlab = "Customers", xaxt = "n",ylab = "Active Duration(seconds)")
+inactiveChart = barplot(userActiveDuration$inactive,name = userActiveDuration$Customer,main="Inactive Duration per User",xlab = "Customers", xaxt = "n",ylab = "Inactive Duration(seconds)")
+activityChart = ggplot(userActiveDuration, aes(fill=inactive, y=active, x = Customer)) + 
+  geom_bar(position = "stack",stat="identity")
+activityChart + labs(y="Active Duration(seconds)", x = "Customer #")+ ggtitle("Active/Inactive duration per Customer")
 
+#charts for user frequency
+frequency = aggregate(data$login_date~data$customer_id, unique(data), FUN=list)
+lengths(frequency$`data$login_date`, use.names = TRUE)
+frequency$freq <- lengths(frequency$`data$login_date`, use.names = TRUE)
+frequencyChart = barplot(height = frequency$freq,names = frequency$`data$customer_id`,main="Visit Frequency",xlab = "Customers", xaxt = "n",ylab = "Number of Unique Visits")
+frequencyChart + theme(axis.title.x = element_blank())
+mean(frequency$freq)
